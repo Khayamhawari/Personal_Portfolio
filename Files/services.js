5c909a771635dcd54c8accf81f9644a96fded971
Md Khayam Hawari <khayamraza92@gmail.com>
2024-08-03 09:49:42 +0530

    Add features

--- Files/services.js
+++ Files/services.js
@@ -1,3 +1,4 @@
+//humbarger menu open and close
 const menuOpen = document.getElementById('open-menu');
         const navBar = document.getElementById('main-nav');
 
@@ -5,3 +6,36 @@ const menuOpen = document.getElementById('open-menu');
             menuOpen.classList.toggle('openmenu');
             navBar.classList.toggle('active');
         });
+
+//theme mode - light and dark theme
+let lightMode = localStorage.getItem('lightMode');
+const themeIcon = document.getElementById('theme-icon');
+const header = document.getElementById('header');
+
+const enableLightTheme = () => {
+  document.body.classList.add('light-theme');
+  themeIcon.src = 'Assets/moon.png';
+  header.style.backgroundColor = 'rgba(255, 255, 255, 0.507)';
+  localStorage.setItem('lightMode', 'enable');
+}
+
+const disableLightTheme = () => {
+  document.body.classList.remove('light-theme');
+  themeIcon.src = 'Assets/sun.png';
+  header.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+  localStorage.setItem('lightMode', 'disable');
+}
+
+if (lightMode === 'enable') {
+  enableLightTheme();
+}
+
+themeIcon.addEventListener('click', () => {
+  lightMode = localStorage.getItem('lightMode');
+  if (lightMode !== 'enable') {
+    enableLightTheme();
+  } else {
+    disableLightTheme();
+  }
+
+})        
