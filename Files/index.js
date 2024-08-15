d4fa6c2fa1ad5aaffdca61de53dfe43520c22570
Md Khayam Hawari <khayamraza92@gmail.com>
2024-08-15 15:38:21 +0530

    some change

--- Files/index.js
+++ Files/index.js
@@ -118,9 +118,9 @@ form.addEventListener("submit", (e) => {
 
 
 // contextmenu hide from website looks like professional
-// document.addEventListener("contextmenu", (c)=>{
-//    c.preventDefault();
-// },false);
+document.addEventListener("contextmenu", (c)=>{
+   c.preventDefault();
+},false);
 
 
 
