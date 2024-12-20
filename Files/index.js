240ec98d4345df8f3c0ac2cf41ff851ed60606d3
Md Khayam Hawari <khayamraza92@gmail.com>
2024-12-21 00:56:26 +0530

    Updated resume added

--- Files/index.js
+++ Files/index.js
@@ -238,3 +238,16 @@ tabs.forEach((tab, index) => {
   });
 });
 
+//<!--Start of Tawk.to Script-->
+//<script type="text/javascript">
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/675a6df649e2fd8dfef6c03d/1iesjr3m6';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
+//</script>
+//<!--End of Tawk.to Script-->
